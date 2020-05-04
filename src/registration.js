40f3401a8bf191e209316f8a9da790952a05b615
ANGELIKA KIM <alika852@gmail.com>
2020-05-04 12:48:06 +0200

    error message not displayed

--- src/registration.js
+++ src/registration.js
@@ -22,23 +22,29 @@ export default class Registration extends React.Component {
     }
 
     submit() {
-        console.log("about to submit", this.state);
         axios
             .post("/register", this.state)
             .then(({ data }) => {
                 // need route on server
-                console.log("response: ", data);
+                console.log("****response: ", data);
                 if (data.success) {
+                    console.log("registration success");
+                    this.setState({
+                        error: false,
+                    });
                     location.replace("/");
                 } else {
+                    console.log("registration failure");
                     this.setState({
                         error: true,
                     });
+                    console.log("in axios in submit", this.state);
                 }
             })
             .catch((err) => {
                 console.log("Error in axios.post '/register': ", err);
             });
+        console.log("after axios in submit", this.state);
     }
 
     render() {
